6d7077ebab35573333f425903b09c5a54d841269
johng <jcguest90@gmail.com>
2020-08-17 00:15:40 -0500

    redux-thunk and user reducer

---
+++ client/src/reducer.js
@@ -0,0 +1,36 @@
+export default function reducer( state = { isLoggedIn: false, loading: false, user: {}}, action) {
+    switch (action.type) {
+        case "LOADING_STATUS":
+            return {
+                isLoggedIn: false,
+                user: {},
+                loading: true
+            }
+        case "LOG_IN":
+            return {
+                isLoggedIn: action.isLoggedIn,
+                user: action.user,
+                loading: false
+
+        }
+        case "LOG_OUT":
+            return {
+                isLoggedIn: action.isLoggedIn,
+                user: action.user,
+                loading: false
+            }
+        case "LOADING_ITEMS":
+            return {
+                ...state,
+                loading: true
+            }
+        case "ADD_ITEMS":
+            return {
+                ...state,
+                items: action.items,
+                loading: action.loading
+            }
+        default: return state;
+
+    }
+}
